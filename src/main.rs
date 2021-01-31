e94b9a07a09c315223dcc30353f9be56b0d0a005
Han Hossain <ahanuf12@gmail.com>
2021-01-31 14:55:23 -0700

    tide hello world

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,26 @@
 use crate::database::Database;
+use tide::prelude::*;
+use tide::Request;
 
 mod database;
 mod error;
 mod models;
 
+#[derive(Debug, Deserialize)]
+struct Animal {
+    name: String,
+    legs: u8,
+}
+
 #[async_std::main]
-async fn main() -> Result<(), anyhow::Error> {
-    let db = Database::connect().await?;
-
-    println!("----- Users -----");
-
-    let users = db.get_users().await?;
-    for user in users {
-        println!("{} - {}", user.id, user.username);
-    }
-
-    println!("----- Creating User -----");
-    db.create_user("john").await?;
-
-    println!("----- Users -----");
-    let users = db.get_users().await?;
-    for user in users {
-        println!("{} - {}", user.id, user.username);
-    }
-
+async fn main() -> tide::Result<()> {
+    let mut app = tide::new();
+    app.at("/orders/shoes").post(order_shoes);
+    app.listen("127.0.0.1:8080").await?;
     Ok(())
+}
+
+async fn order_shoes(mut req: Request<()>) -> tide::Result {
+    let Animal { name, legs } = req.body_json().await?;
+    Ok(format!("Hello, {}! I've put in an order for {} shoes", name, legs).into())
 }
