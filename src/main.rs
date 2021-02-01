32e6dca7561f60f768c107a94ba4221e1460223d
Han Hossain <ahanuf12@gmail.com>
2021-01-31 19:44:25 -0700

    implement get/create users

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,42 @@
 use crate::database::Database;
+use crate::models::UserNoId;
 use tide::prelude::*;
-use tide::Request;
+use tide::{Body, Request, Response, StatusCode};
 
 mod database;
 mod error;
 mod models;
 
-#[derive(Debug, Deserialize)]
-struct Animal {
-    name: String,
-    legs: u8,
+#[derive(Clone)]
+struct State {
+    database: Database,
 }
 
 #[async_std::main]
 async fn main() -> tide::Result<()> {
-    let mut app = tide::new();
-    app.at("/orders/shoes").post(order_shoes);
+    let database = Database::connect().await?;
+    let mut app = tide::with_state(State { database });
+    app.at("/api/users").get(get_users).post(create_user);
     app.listen("127.0.0.1:8080").await?;
     Ok(())
 }
 
-async fn order_shoes(mut req: Request<()>) -> tide::Result {
-    let Animal { name, legs } = req.body_json().await?;
-    Ok(format!("Hello, {}! I've put in an order for {} shoes", name, legs).into())
+async fn get_users(req: Request<State>) -> tide::Result {
+    let users = req.state().database.get_users().await?;
+    let response = Response::builder(StatusCode::Ok)
+        .body(Body::from_json(&users)?)
+        .build();
+
+    Ok(response)
+}
+
+async fn create_user(mut req: Request<State>) -> tide::Result {
+    let user = req.body_json::<UserNoId>().await?;
+
+    let user = req.state().database.create_user(&user.username).await?;
+    let response = Response::builder(StatusCode::Created)
+        .body(Body::from_json(&user)?)
+        .build();
+
+    Ok(response)
 }
